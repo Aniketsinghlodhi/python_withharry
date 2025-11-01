db5f013dd5987434be8fa1ede3302311c98781c9
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-01 12:00:58 +0530

    04_typecating

---
+++ 04_typecasting/01_typecasting.py
@@ -0,0 +1,86 @@
+a = 34 
+b = "56" 
+# Typecasting is a process of converting one data type to another data type. 
+#python provides several built-in functions for typecasting, such as int(), float(), str(), etc.
+
+
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+# Typecasting b to integer
+b = int(b)
+print(b)
+print(type(b))  
+# Now we can add a and b
+c = a + b
+print(c)
+print(type(c))
+
+
+# Typecasting a to string
+a = str(a)
+print(a)
+print(type(a))
+# Now we can concatenate a and b        
+c = a + str(b)
+print(c)
+print(type(c))  
+
+
+# Typecasting b to float
+b = float(b)
+print(b)
+print(type(b))
+# Now we can add a and b
+a = float(a)
+c = a + b
+print(c)
+print(type(c))  
+
+
+# Typecasting a to integer
+a = int(a)
+print(a)
+print(type(a))
+# Now we can add a and b
+b = int(b)
+c = a + b
+print(c)
+print(type(c)) 
+
+
+
+# Typecasting b to string
+b = str(b)          
+print(b)
+print(type(b))
+# Now we can concatenate a and b        
+a = str(a)
+c = a + b
+print(c)
+print(type(c))  
+
+
+# Typecasting a to float
+a = float(a)
+print(a)
+print(type(a))
+# Now we can add a and b
+b = float(b)
+c = a + b
+print(c)
+print(type(c))  
+
+# Typecasting b to integer
+b = int(b)
+print(b)
+print(type(b))  
+# Now we can add a and b
+a = int(a)
+c = a + b
+print(c)
+print(type(c))      
+
