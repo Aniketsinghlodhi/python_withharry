aaac455e25a934a4ff37d5940246a9f29eb2523b
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-01 12:44:50 +0530

    User_input

---
+++ 05_user_input/02_add.py
@@ -0,0 +1,7 @@
+a =  int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+# a = int(a)  this is another way to make code more readable and small 
+# b = int(b)
+# input by default takes string input so we have to convert it into integer 
+sum = print (a + b)
+
