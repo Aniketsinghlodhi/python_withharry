bcab193dd25e929feafed2aee903336d4567a484
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-02 21:53:32 +0530

    About stings

---
+++ 10_strings/01_string.py
@@ -0,0 +1,5 @@
+name = "Aniketsinghlodhi"
+name = 'Aniketsinghlodh'
+name = '''Aniket is a very good and curious boy , what's your opinion on Aniket that he is what type of personaliyt .
+'''
+print(name)
