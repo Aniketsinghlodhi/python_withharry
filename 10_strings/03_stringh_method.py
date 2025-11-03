79e51e671e07e08cc238792e7bbe27942d9feea8
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-03 08:02:42 +0530

    uncomments all codes

--- 10_strings/03_stringh_method.py
+++ 10_strings/03_stringh_method.py
@@ -1,25 +1,25 @@
-    # stings are immutable
-# name = "Aniketsinghlodhi"
-# # name[0] = "S"
-# # alteration isn't possible in string , we cannot do this 
-# a = len(name)
-# print(a)
-# print(name.upper() , name)
-# print(name.lower() , name)
-# print(name.capitalize() , name)
-# print(name.title() , name)
+  #  stings are immutable
+name = "Aniketsinghlodhi"
+# name[0] = "S"
+# alteration isn't possible in string , we cannot do this 
+a = len(name)
+print(a)
+print(name.upper() , name)
+print(name.lower() , name)
+print(name.capitalize() , name)
+print(name.title() , name)
 
-# # Removing Whitespace
-# text = "   Hello Aniket.  "
-# print(text.strip())
-# print(text.lstrip())
-# print(text.rstrip())
+# Removing Whitespace
+text = "   Hello Aniket.  "
+print(text.strip())
+print(text.lstrip())
+print(text.rstrip())
 
 
-# # Finding And Replacking 
-# text = "Python is fun "
-# print(text.find("is")) # output is 7 index of first occurance
-# print(text.replace("fun", "Awesome"))
+# Finding And Replacking 
+text = "Python is fun "
+print(text.find("is")) # output is 7 index of first occurance
+print(text.replace("fun", "Awesome"))
 
 #Splitting And joining 
 fruits = "Apple, Bananas, Pineapples, Mangoes, Dates, Plum , Grapes " 
