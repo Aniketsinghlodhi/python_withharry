c5c3e9743e082f6a5769b8ed27aba207db15a065
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-04 20:55:16 +0530

    functions

--- 11_Functions/01_function.py
+++ 11_Functions/01_function.py
@@ -1,10 +1,10 @@
 # function definition 
 # function help in reusability and modularity in python. 
 # Syntax: 
-# def greet(name):
-#     return f"Hello, {name}"
+def greet(name):
+    return f"Hello, {name}"
 
-# print(greet("Aniket" ))
+print(greet("Aniket" ))
 
 # key points 
 # Defineed using def keyword. 
@@ -13,18 +13,18 @@
    
 
 
-# a = 12
-# b = 2 
-# c = 4 
+a = 12
+b = 2 
+c = 4 
 
-# average = (a+b+c)/3
-# print(average)
+average = (a+b+c)/3
+print(average)
 
 
-# a1 = 13 
-# b1 = 2 
-# c1 = 1 
-# average = (a1+b1+c1)/3
+a1 = 13 
+b1 = 2 
+c1 = 1 
+average = (a1+b1+c1)/3
 # print(average)
 
 def average(a,b,c):
@@ -35,3 +35,4 @@ def average(a,b,c):
     
 b = average(2,3,4)
 print(b)
+ 
