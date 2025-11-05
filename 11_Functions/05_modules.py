c02b1411834991935c7a1ecf4446ccc8d0c001b1
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-05 14:02:35 +0530

    modules

---
+++ 11_Functions/05_modules.py
@@ -0,0 +1,7 @@
+# Importing Modules
+# Python provides built-in and third-party modules.
+# There are two types of module in python one is build in modules and second one is external module 
+# Example: Using the math module
+import math # Built-in module
+
+print(math.sqrt(16))  # Output: 4.0
