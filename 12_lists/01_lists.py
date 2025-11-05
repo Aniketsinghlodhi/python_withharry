2c89b69b604eece5b213068da9e5bbec03dd7320
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-05 23:56:45 +0530

    lists

---
+++ 12_lists/01_lists.py
@@ -0,0 +1,19 @@
+# Introduction to Lists
+# Lists are ordered, mutable (changeable) collections of items.
+# lists can contains various type of data like num, strins , set 
+
+marks = [ 54,45,98,78,90,87,67]
+mixed = [ "aniket" ,4 , False, 46 ,True ]
+
+print(marks[0])
+print(mixed)
+print(marks[3])
+print(marks[2:4])
+print(marks[2:4])
+print(marks[2:3])
+print(marks[2:5])
+print(marks[2:6])
+print(marks[2:7])
+print(marks[2:8])
+print(marks[2:])
+print(marks[2:9])
