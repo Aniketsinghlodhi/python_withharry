35ae1e71409e49932d90f8547569136ac32f5ba9
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-07 01:54:14 +0530

    practive_set_python_collection

---
+++ practice_set_python_collection_05/problem_1.1.py
@@ -0,0 +1,7 @@
+fruits = ["aaple", "Banana ", "cherry", "pineapple"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(len(fruits))
---
+++ practice_set_python_collection_05/problem_2.1.py
@@ -0,0 +1,5 @@
+numbers = [4,7,9,3,5,2,]
+numbers.sort()
+numbers.append(10)
+numbers.remove(2)
+print(numbers)
