6a5c63c7d1c638000240a53800e47bcfe90b4ab8
Aniketsinghlodhi <aniketlodhi@icloud.com>
2025-11-09 21:45:58 +0530

    decorators_with_arguments

--- 17_python_advanced_concepts/02_dec_with_args.py
+++ 17_python_advanced_concepts/02_dec_with_args.py
@@ -1,7 +1,11 @@
+
+# Decorators themselves can also accept arguments. This requires another level of nesting: an outer function that takes the decorator's arguments and returns the actual decorator function.
+
 def repeat(n):
     def decorator(func):
         def  wrapper(a):
             for i in range (n):
+                
                 func(a)
         return wrapper
     return decorator
@@ -10,5 +14,15 @@ def repeat(n):
 @repeat(7)
 def say_hello(a):
     print(f"hello!{a}")
+
+    '''
+    it replaces the function say_hello with this 
+    def decorator(func):
+        def  wrapper(a):
+            for i in range (n):
+                func(a)
+        return wrapper
+    
+    '''
 
 say_hello("Aniketsingh")
